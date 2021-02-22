27d3fb9b40d9539f661f4bb66829518a2cabec3a
Alexander Bubnov <alexander.bubnov@gmail.com>
2021-02-22 21:09:46 +0300

    .

---
+++ задачи_по_программированию/binarytree/main.go
@@ -0,0 +1,58 @@
+package main
+
+func main() {
+}
+
+type BTree struct {
+	tree *_Tree
+}
+
+type _Tree struct {
+	Left  *_Tree // smaller
+	Right *_Tree // bigger
+	Data  *_Data
+}
+
+type _Data struct {
+	N int
+}
+
+func (b *BTree) Add(d *_Data) {
+	ntree := &_Tree{Data: d}
+	if b.tree == nil {
+		b.tree = ntree
+		return
+	}
+
+	for tree := b.tree; ; {
+		if tree.Data.Equal(d) {
+			return
+		}
+
+		if tree.Data.More(d) {
+			if tree.Right == nil {
+				tree.Right = ntree
+				return
+			}
+			tree = tree.Right
+		} else if tree.Data.Less(d) {
+			if tree.Left == nil {
+				tree.Left = ntree
+				return
+			}
+			tree = tree.Left
+		}
+	}
+}
+
+func (d *_Data) Equal(n *_Data) bool {
+	return d.N == n.N
+}
+
+func (d *_Data) More(n *_Data) bool {
+	return d.N > n.N
+}
+
+func (d *_Data) Less(n *_Data) bool {
+	return d.N < n.N
+}

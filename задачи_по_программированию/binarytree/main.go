6fb4f003ce88ed80d6f9064ff4474b1bbeab5aac
Alexander Bubnov <alexander.bubnov@gmail.com>
2021-02-24 10:00:42 +0300

    .

--- задачи_по_программированию/binarytree/main.go
+++ задачи_по_программированию/binarytree/main.go
@@ -45,6 +45,22 @@ func (b *BTree) Add(d *_Data) {
 	}
 }
 
+func (b *BTree) Exists(d *_Data) bool {
+	for tree := b.tree; tree != nil; {
+		if tree.Data.Equal(d) {
+			return true
+		}
+
+		if tree.Data.More(d) {
+			tree = tree.Right
+		} else if tree.Data.Less(d) {
+			tree = tree.Left
+		}
+	}
+
+	return false
+}
+
 func (d *_Data) Equal(n *_Data) bool {
 	return d.N == n.N
 }
